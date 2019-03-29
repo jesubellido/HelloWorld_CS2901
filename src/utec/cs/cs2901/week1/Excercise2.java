7272299923c665d1cea2ce2cc2e94ffd41f5179e
Rodrigo Bedoya <rodrigo.bedoya@utec.edu.pe>
2019-03-29 12:02:37 -0500

    fixed arr size

--- src/utec/cs/cs2901/week1/Excercise2.java
+++ src/utec/cs/cs2901/week1/Excercise2.java
@@ -2,8 +2,8 @@ package utec.cs.cs2901.week1;
 
 public class Excercise2 {
     public static void main(String[] args) {
-        int[] arr = new int[5];
-        for(int i = 0; i <= 5; i++) {
+        Integer[] arr = new Integer[5];
+        for(int i = 0; i <= 4; i++) {
             arr[i] = i;
         }
         System.out.println(arr);
