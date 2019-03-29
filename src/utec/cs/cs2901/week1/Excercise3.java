6d01e708ff759bfdb5c030b52b6ec52b90a1bbfe
utec <jorgemayna@gmail.com>
2019-03-29 12:06:58 -0500

    fix execise 3

--- src/utec/cs/cs2901/week1/Excercise3.java
+++ src/utec/cs/cs2901/week1/Excercise3.java
@@ -4,7 +4,9 @@ public class Excercise3 {
     public static void main(String[] args) {
         String[] arr = {"The", "quick", "brown", "fox", "jumped", "over", "the", null};
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i].toLowerCase());
+            if(arr[i]!=null){
+                System.out.println(arr[i].toLowerCase());
+            }
         }
     }
 }
