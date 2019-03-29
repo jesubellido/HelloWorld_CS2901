3a8bb47622d420231b5dacec13bb2630e16e2253
jotarios <j29.rios@gmail.com>
2019-03-29 12:16:22 -0500

    Add ex4

--- src/utec/cs/cs2901/week1/Excercise4.java
+++ src/utec/cs/cs2901/week1/Excercise4.java
@@ -2,9 +2,10 @@ package utec.cs.cs2901.week1;
 
 public class Excercise4 {
     private static String[] texts = new String[]{"find","the","bug"};
+
     private static boolean contains(String text){
         for(String item : texts){
-            if( item == text.toLowerCase() ){
+            if(item.equalsIgnoreCase(text)){
                 return true;
             }
         }
