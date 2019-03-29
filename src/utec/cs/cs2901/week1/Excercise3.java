af432e9171e968dff4f5e4f488ddc370c5a13e68
fsocualaya <fernando.socualaya@utec.edu.pe>
2019-03-29 12:08:30 -0500

    Fixed issues in Exercise 3

--- src/utec/cs/cs2901/week1/Excercise3.java
+++ src/utec/cs/cs2901/week1/Excercise3.java
@@ -3,7 +3,7 @@ package utec.cs.cs2901.week1;
 public class Excercise3 {
     public static void main(String[] args) {
         String[] arr = {"The", "quick", "brown", "fox", "jumped", "over", "the", null};
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length-1; i++) {
             System.out.println(arr[i].toLowerCase());
         }
     }
