16783eab014e823ae3d19be6d47eca8337b1d493
Arturo Cuya <arturocuya099@gmail.com>
2019-03-29 12:09:03 -0500

    fix ex. 2 & 3

--- src/utec/cs/cs2901/week1/Excercise3.java
+++ src/utec/cs/cs2901/week1/Excercise3.java
@@ -4,7 +4,9 @@ public class Excercise3 {
     public static void main(String[] args) {
         String[] arr = {"The", "quick", "brown", "fox", "jumped", "over", "the", null};
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i].toLowerCase());
+            try {
+                System.out.println(arr[i].toLowerCase());
+            } catch(Exception ignored) {}
         }
     }
 }
