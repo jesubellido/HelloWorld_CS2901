470d45fbb7d24a71d10ed433c5ba32028e882c70
Antonio <antonio.toche@utec.edu.pe>
2019-03-29 12:08:03 -0500

    bugs fixes

--- src/utec/cs/cs2901/week1/Excercise3.java
+++ src/utec/cs/cs2901/week1/Excercise3.java
@@ -1,5 +1,5 @@
 package utec.cs.cs2901.week1;
-
+//bugs fixes
 public class Excercise3 {
     public static void main(String[] args) {
         String[] arr = {"The", "quick", "brown", "fox", "jumped", "over", "the", };
