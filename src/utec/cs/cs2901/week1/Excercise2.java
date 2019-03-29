ab19b62db187d9e0efae2b21083a61fa17b855c7
L4mb0 <juan.navarro@utec.edu.pe>
2019-03-29 12:31:30 -0500

    Ejercicios coregidos JMNavarro

--- src/utec/cs/cs2901/week1/Excercise2.java
+++ src/utec/cs/cs2901/week1/Excercise2.java
@@ -3,7 +3,7 @@ package utec.cs.cs2901.week1;
 public class Excercise2 {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        for(int i = 0; i <= 5; i++) {
+        for(int i = 0; i < 5; i++) {
             arr[i] = i;
         }
         System.out.println(arr);
