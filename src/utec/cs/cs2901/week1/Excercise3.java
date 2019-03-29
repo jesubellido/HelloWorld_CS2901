fc0d3da008602e9b7d9243fdd18f417e8b6ee1c2
jotarios <j29.rios@gmail.com>
2019-03-29 12:08:30 -0500

    Add ex3

--- src/utec/cs/cs2901/week1/Excercise3.java
+++ src/utec/cs/cs2901/week1/Excercise3.java
@@ -3,8 +3,9 @@ package utec.cs.cs2901.week1;
 public class Excercise3 {
     public static void main(String[] args) {
         String[] arr = {"The", "quick", "brown", "fox", "jumped", "over", "the", null};
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i].toLowerCase());
-        }
+        for(int i = 0; i < arr.length; i++)
+            if (arr[i] != null) {
+                System.out.println(arr[i].toLowerCase());
+            }
     }
 }
