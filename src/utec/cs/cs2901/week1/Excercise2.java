e012ce221bfc540797b3ee670882ef3afa526c26
Rodrigo Bedoya <rodrigo.bedoya@utec.edu.pe>
2019-03-29 13:21:52 -0500

    implemented review requests

--- src/utec/cs/cs2901/week1/Excercise2.java
+++ src/utec/cs/cs2901/week1/Excercise2.java
@@ -2,8 +2,8 @@ package utec.cs.cs2901.week1;
 
 public class Excercise2 {
     public static void main(String[] args) {
-        Integer[] arr = new Integer[5];
-        for(int i = 0; i <= 4; i++) {
+        int[] arr = new int[5];
+        for(int i = 0; i < 5; i++) {
             arr[i] = i;
         }
         System.out.println(arr);
