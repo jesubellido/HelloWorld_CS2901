c01755de8d9fc18f46076f25805fbf83ab7a5d35
Antonio <antonio.toche@utec.edu.pe>
2019-03-29 12:03:16 -0500

    add Integer, Bug's fixes

--- src/utec/cs/cs2901/week1/Excercise2.java
+++ src/utec/cs/cs2901/week1/Excercise2.java
@@ -2,7 +2,7 @@ package utec.cs.cs2901.week1;
 
 public class Excercise2 {
     public static void main(String[] args) {
-        int[] arr = new int[5];
+        Integer[] arr = new Integer[5];
         for(int i = 0; i <= 4; i++) {
             arr[i] = i;
         }
