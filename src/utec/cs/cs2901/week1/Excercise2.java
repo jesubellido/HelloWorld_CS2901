a71ebd8d08abd18fd8392423a7da373d81e990e3
utec <ian.ariasschreiber@utec.edu.pe>
2019-03-29 12:45:47 -0500

    Ejercicio 1,2,3,4,5

--- src/utec/cs/cs2901/week1/Excercise2.java
+++ src/utec/cs/cs2901/week1/Excercise2.java
@@ -1,11 +1,13 @@
 package utec.cs.cs2901.week1;
 
+import java.util.Arrays;
+
 public class Excercise2 {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        for(int i = 0; i <= 5; i++) {
+        for(int i = 0; i < 5; i++) {
             arr[i] = i;
         }
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/utec/cs/cs2901/week1/Excercise3.java
+++ src/utec/cs/cs2901/week1/Excercise3.java
@@ -1,10 +1,20 @@
 package utec.cs.cs2901.week1;
 
-public class Excercise3 {
-    public static void main(String[] args) {
+public class Excercise3
+{
+    public static void main(String[] args)
+    {
         String[] arr = {"The", "quick", "brown", "fox", "jumped", "over", "the", null};
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i].toLowerCase());
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == null)
+            {
+                return;
+            }
+            else
+            {
+                System.out.println(arr[i].toLowerCase());
+            }
         }
     }
 }
